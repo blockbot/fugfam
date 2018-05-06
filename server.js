8206e4b48a79aa07fa5dbef0885496d1c2695aa9
blockbot <blockbot@gmail.com>
2018-05-06 07:39:32 -0400

    first step towards node rendering react

--- server.js
+++ server.js
@@ -2,11 +2,21 @@ const Koa = require('koa');
 const Router = require('koa-router');
 const app = new Koa();
 const router = new Router();
+const serve = require('koa-static');
+const React = require('react');
+const ReactDOMServer = require('react-dom/server');
+const Html = require(__dirname + '/client/src/Html');
 
 const port = 6969;
 
 router.get('/', ctx => {
-    ctx.body = "farts";
+    const body = "balls";
+    const title = 'Server side Rendering with Styled Components';
+    
+    ctx.body = Html({
+        body,
+        title
+    });
 });
 
 app
@@ -14,3 +24,5 @@ app
     .use(router.allowedMethods());
 
 app.listen(port);
+
+console.log('listening on port: ' + port);
