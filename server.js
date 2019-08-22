c0e34136907ce076a62225db202d636a47bb2685
blockbot <blockbot@gmail.com>
2019-08-22 15:08:20 -0700

    Thanks, I hate it.

--- server.js
+++ server.js
@@ -1,77 +1,18 @@
+const express = require('express')
+const next = require('next')
+const port = parseInt(process.env.PORT, 10) || 80
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handle = app.getRequestHandler()
 
-import "@babel/polyfill";
-import Koa from 'koa';
-import Router from 'koa-router';
-import { renderToString } from 'react-dom/server';
-import serve from 'koa-static';
-import React from 'react';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
-import Html from './client/src/Html';
-import App from './client/src/App';
-import rootReducer from './client/src/reducers';
-import AWS from 'aws-sdk';
+app.prepare().then(() => {
+  const server = express();
 
-const app = new Koa();
-const router = new Router();
+  server.get('*', (req, res) => {
+    return handle(req, res);
+  })
 
-const port = 6969;
-
-router.get('/', ctx => {
-	const store = createStore(rootReducer);
-	const preloadedState = store.getState();
-
-	const body = renderToString(
-		<Provider store={store}>
-			<App activeApp='${preloadedState.apps.activeApp}' />
-		</Provider>
-	);
-	const title = 'FUG NET v1.0';
-
-	ctx.body = Html({
-		body,
-		title,
-		preloadedState
-	});
-});
-
-router.get('/images', async (ctx) => {
-	const region = "sfo2";
-	const spacesEndpoint = `${region}.digitaloceanspaces.com`;
-	const awsCreds = {
-			accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-			endpoint: spacesEndpoint,
-			secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-	};
-
-	const s3 = new AWS.S3(awsCreds);
-	const params = {
-			Bucket: getQueryParam(ctx.querystring, 'bucket'),
-	};
-	const images = {data: []};
-
-	await s3.listObjectsV2(params).promise().then((data) => {
-		data.Contents.forEach((content) => {
-			images.data.push(content);
-		});
-	}).catch((err) => {
-		console.log("err: ", err);
-	});
-
-	ctx.body = images;
-});
-
-function getQueryParam(string, param) {
-	var regex = new RegExp(param + '=([^&#]*)');
-	return regex.exec(string)[1];
-}
-
-app
-	.use(router.routes())
-	.use(router.allowedMethods())
-	.use(serve('./dist'))
-	.use(serve('./assets/images'));
-
-app.listen(port);
-
-console.log('listening on port: ' + port);
+  server.listen(port, () => {
+    console.log(`FUGNET online at http://localhost:${port}`);
+  });
+})
