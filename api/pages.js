1650cb011c1319fe43d1313f0b0c141f0a61445c
blockbot <blockbot@gmail.com>
2021-03-24 10:40:55 -0700

    thing update

--- api/pages.js
+++ api/pages.js
@@ -3,13 +3,15 @@ import GhostContentAPI from "@tryghost/content-api";
 const api = new GhostContentAPI({
   url: 'https://joeydehnert.ghost.io',
   key: 'c87a900be098ea0deb27785d08',
-  version: "v3"
+  version: "v3",
+  thing: "thing"
 });
 
 export async function getPageFromSlug(slug) {
   return await api.pages
     .read({
-      slug: slug
+      slug: slug,
+      thing: thing
     })
     .catch(err => {
       console.error(err);
