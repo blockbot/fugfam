331d27ed3abc56db802ebb6208ba1aa4ce3b648a
blockbot <blockbot@gmail.com>
2021-03-24 10:44:00 -0700

    TICKET-222: removed thing

--- api/pages.js
+++ api/pages.js
@@ -3,15 +3,13 @@ import GhostContentAPI from "@tryghost/content-api";
 const api = new GhostContentAPI({
   url: 'https://joeydehnert.ghost.io',
   key: 'c87a900be098ea0deb27785d08',
-  version: "v3",
-  thing: "thing"
+  version: "v3"
 });
 
 export async function getPageFromSlug(slug) {
   return await api.pages
     .read({
-      slug: slug,
-      thing: thing
+      slug: slug
     })
     .catch(err => {
       console.error(err);
